294e4f439e9476ef9613a923466896fa6cbc15f9
Tereza Z <t.zemanova@seznam.cz>
2023-10-02 21:03:42 +0200

    proslo

---
+++ ukol 01.py
@@ -0,0 +1,6 @@
+jmeno = (input("Zadejte jmeno: "))
+print (f"{jmeno}@czechitas.cz")
+
+jmeno_a_prijmeni = str(input("Zadejte jmeno a prijmeni: "))
+print (f"{jmeno_a_prijmeni}".upper())
+print (f"{jmeno_a_prijmeni}".lower())
